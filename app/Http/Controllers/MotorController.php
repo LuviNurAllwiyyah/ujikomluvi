c20f2b9939b01f9a386cf93bbc9591a7f6cec2ed
faricfh <ifake377@gmail.com>
2019-08-10 13:23:34 +0700

    pesan

--- app/Http/Controllers/MotorController.php
+++ app/Http/Controllers/MotorController.php
@@ -3,8 +3,100 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Motor;
 
 class MotorController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $motor = Motor::all();
+        $response = [
+            'success' => true,
+            'data' => $motor,
+            'message' => 'Berhasil disimpan'
+        ];
+        return response()->json(response, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $motor = new Motor;
+        $motor->motor_merk = $request->get('motor_merk');
+        $motor->motor_type = $request->get('motor_type');
+        $motor->motor_warna_pilihan = $request->get('motor_warna_pilihan');
+        $motor->motor_harga = $request->get('motor_harga');
+        $motor->mmotor_gambar = $request->get('motor_gambar');
+        $response = [
+            'success' => true,
+            'data' => $motor,
+            'message'=> 'Berhasil disimpan'
+        ];
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' =>false]);
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
